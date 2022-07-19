303254701d88cd9b404b663a47f72e9c3b92672c
Andres Chacon <andres_men98@hotmail.com>
2022-07-18 19:20:16 -0500

    feat: adding my picture and my info

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div className="card">
-        <button className="button-visit" onClick={() => setCount((count) => count + 1)}>Contar Visita: {count}</button>
+      <div className='photo'>
+        <img className="picture" src="./img/me.jpeg" alt="perfil"></img>
       </div>
+      <div className='name'>
+        <h1>Jesús Andrés Chacón Hernández</h1>
+      </div>
+      <div className='information'>
+        <p>Al finalizar la carrera me gustaría encontrar un buen trabajo referente a esto y no fracasar</p>
+        <p>para asi poder tener un buen sustento economico y no fallar en el intento.</p>
+      </div>
+      <div className="card">
+        <button className="button-visit" onClick={() => setCount((count) => count + 1)}>Contar Visita: </button>
+      </div>
+      <div>
+        <p>Contador de visitas: {count}</p>
+      </div>
+
     </div>
   )
 }
-
 export default App
